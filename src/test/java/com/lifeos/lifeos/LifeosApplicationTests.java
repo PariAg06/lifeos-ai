2c88b9e54568e0b89d56d29349d9aa1425cab929
PariAg06 <124270057+PariAg06@users.noreply.github.com>
2026-09-20 21:20:57 +0530

    Initial Spring Boot project setup

---
+++ src/test/java/com/lifeos/lifeos/LifeosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lifeos.lifeos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifeosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
